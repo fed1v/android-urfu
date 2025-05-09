64b1409238adb5852e7a98724724bc19218e7175
fed1v <fedivmi@gmail.com>
2025-05-09 18:38:52 +0500

    protocols/01-sql: example 6 done (https://metanit.com/java/android/14.6.php)

--- app/src/main/java/com/shurdev/androidurfu/task01sql/UserActivity.java
+++ app/src/main/java/com/shurdev/androidurfu/task01sql/UserActivity.java
@@ -1,9 +1,6 @@
 package com.shurdev.androidurfu.task01sql;
 
-import android.content.ContentValues;
 import android.content.Intent;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,16 +12,12 @@ import com.shurdev.androidurfu.R;
 
 public class UserActivity extends AppCompatActivity {
 
-    EditText nameBox;
-    EditText yearBox;
-    Button delButton;
-    Button saveButton;
+    private EditText nameBox;
+    private EditText yearBox;
+    private Button delButton;
 
-    DatabaseHelper sqlHelper;
-    SQLiteDatabase db;
-    Cursor userCursor;
-    long userId = 0;
-
+    private DatabaseAdapter adapter;
+    private long userId=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,10 +26,7 @@ public class UserActivity extends AppCompatActivity {
         nameBox = findViewById(R.id.name);
         yearBox = findViewById(R.id.year);
         delButton = findViewById(R.id.deleteButton);
-        saveButton = findViewById(R.id.saveButton);
-
-        sqlHelper = new DatabaseHelper(this);
-        db = sqlHelper.open();
+        adapter = new DatabaseAdapter(this);
 
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
@@ -45,39 +35,40 @@ public class UserActivity extends AppCompatActivity {
         // если 0, то добавление
         if (userId > 0) {
             // получаем элемент по id из бд
-            userCursor = db.rawQuery("select * from " + DatabaseHelper.TABLE + " where " +
-                    DatabaseHelper.COLUMN_ID + "=?", new String[]{String.valueOf(userId)});
-            userCursor.moveToFirst();
-            nameBox.setText(userCursor.getString(1));
-            yearBox.setText(String.valueOf(userCursor.getInt(2)));
-            userCursor.close();
+            adapter.open();
+            User user = adapter.getUser(userId);
+            nameBox.setText(user.getName());
+            yearBox.setText(String.valueOf(user.getYear()));
+            adapter.close();
         } else {
             // скрываем кнопку удаления
             delButton.setVisibility(View.GONE);
         }
     }
 
-    public void save(View view) {
-        ContentValues cv = new ContentValues();
-        cv.put(DatabaseHelper.COLUMN_NAME, nameBox.getText().toString());
-        cv.put(DatabaseHelper.COLUMN_YEAR, Integer.parseInt(yearBox.getText().toString()));
+    public void save(View view){
 
+        String name = nameBox.getText().toString();
+        int year = Integer.parseInt(yearBox.getText().toString());
+        User user = new User(userId, name, year);
+
+        adapter.open();
         if (userId > 0) {
-            db.update(DatabaseHelper.TABLE, cv, DatabaseHelper.COLUMN_ID + "=" + userId, null);
+            adapter.update(user);
         } else {
-            db.insert(DatabaseHelper.TABLE, null, cv);
+            adapter.insert(user);
         }
+        adapter.close();
         goHome();
     }
+    public void delete(View view){
 
-    public void delete(View view) {
-        db.delete(DatabaseHelper.TABLE, "_id = ?", new String[]{String.valueOf(userId)});
+        adapter.open();
+        adapter.delete(userId);
+        adapter.close();
         goHome();
     }
-
-    private void goHome() {
-        // закрываем подключение
-        db.close();
+    private void goHome(){
         // переход к главной activity
         Intent intent = new Intent(this, MainActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
