235e85caf42b43f7940593e3ae549076b2bf830e
fed1v <fedivmi@gmail.com>
2025-05-09 17:41:20 +0500

    front/01-parallelism: example 2 done (https://metanit.com/java/android/10.2.php)

--- app/src/main/java/com/shurdev/androidurfu/front01/MainActivity.java
+++ app/src/main/java/com/shurdev/androidurfu/front01/MainActivity.java
@@ -1,15 +1,10 @@
 package com.shurdev.androidurfu.front01;
 
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.shurdev.androidurfu.R;
-
-import java.util.Calendar;
-
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,28 +12,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        TextView textView = findViewById(R.id.textView);
-        Button button = findViewById(R.id.button);
-
-        button.setOnClickListener(v -> {
-            onButtonClicked(textView);
-        });
-    }
-
-    private void onButtonClicked(TextView textView) {
-        Runnable runnable = () -> {
-            Calendar c = Calendar.getInstance();
-
-            int hours = c.get(Calendar.HOUR_OF_DAY);
-            int minutes = c.get(Calendar.MINUTE);
-            int seconds = c.get(Calendar.SECOND);
-
-            String time = hours + ":" + minutes + ":" + seconds;
-
-            textView.post(() -> textView.setText(time));
-        };
-
-        new Thread(runnable).start();
     }
 }
